39c89d94e85df7069c3352831c260983b8078799
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-25 00:19:56 +0530

    `Update import path for db.config to local file`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
-import connectDB from "configs/db.config";
+import connectDB from "./configs/db.config";
 import errorHandler from "middlewares/errorHandler.middleware";
 import notFoundHandler from "middlewares/notFoundHandler.middleware";
 import authRouter from "routes/auth.routes";
