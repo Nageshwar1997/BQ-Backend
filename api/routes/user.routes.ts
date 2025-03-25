dad18ce4da41cca67e4969b8424fb0117fca9763
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-25 22:36:54 +0530

    Project Started

--- api/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUserById } from "../controllers/user.controller";
-
-const userRouter = Router();
-
-userRouter.get("/", getUserById);
-
-export default userRouter;
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import testRouter from "./routes/test";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// API routes
+app.use("/test", testRouter);
+// Root route
+app.get("/", (_: Request, res: Response) => {
+  res.send("Express + TypeScript Server is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+export default app;
