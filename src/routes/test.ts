c823b4afba7fe3be9c0a56098c0476eb6ce1ccc6
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-25 22:46:09 +0530

    All Project Copied

--- src/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-
-const testRouter = Router();
-
-testRouter.get("/", (_: Request, res: Response) => {
-  res.json({ message: "Hello from TypeScript!" });
-});
-
-export default testRouter;
