7e72c26204213949e673ffbae1a5b17466fbbc13
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-27 01:42:57 +0530

    `Added error handling middleware and not found handler to the API routes`

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { CatchErrorResponse, SuccessResponse } from "./utils";
 import connectDB from "./configs/db.config";
 import { AppError } from "./constructors";
 import router from "./routes";
+import errorHandler from "./middlewares/errorHandler.middleware";
+import notFoundHandler from "./middlewares/notFoundHandler.middleware";
 
 const app = express();
 const PORT = process.env.PORT || 5454;
@@ -52,7 +54,14 @@ app.get("/", (_: Request, res: Response) => {
   SuccessResponse(res, 200, "Welcome to the MERN Beautinique API");
 });
 
+// All api routes
 app.use("/api", router);
+
+// Catch undefined routes or routes that don't exist
+app.use(notFoundHandler);
+
+// Error handling middleware
+app.use(errorHandler);
 
 if (process.env.NODE_ENV === "development") {
   app.listen(PORT, async () => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import authRouter from "./auth.routes";
+import blogRouter from "./blog.routes";
+import mediaRouter from "./mediaFiles.routes";
+import userRouter from "./user.routes";
 
 const router = Router();
+
+// Auth routes
+router.use("/auth", authRouter);
+
+// User routes
+router.use("/user", userRouter);
+
+// Media routes
+router.use("/media", mediaRouter);
+
+// Blog routes
+router.use("/blogs", blogRouter);
 
 export default router;
