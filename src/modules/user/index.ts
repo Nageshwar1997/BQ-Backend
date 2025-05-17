66a7383a2f3df27c2aef1984ed60e17e06593565
nageshwarp1997 <nageshwar@ctruh.com>
2025-05-17 14:24:06 +0530

    Added User Module

---
+++ src/modules/user/index.ts
@@ -0,0 +1,4 @@
+export * as Types from "./types";
+export * as Schemas from "./schemas";
+export * as Models from "./models";
+export * as Services from "./services";
---
+++ src/modules/user/services/index.ts
@@ -0,0 +1,56 @@
+import { Models } from "..";
+import { Common } from "../../..";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await Models.User.findOne({ email }).lean();
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserByPhoneNumber = async (phoneNumber: string) => {
+  try {
+    const user = await Models.User.findOne({ phoneNumber }).lean();
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserByEmailOrPhoneNumber = async (
+  email: string,
+  phoneNumber: string
+) => {
+  try {
+    const user = await Models.User.findOne({
+      $or: [{ email }, { phoneNumber }],
+    }).lean();
+
+    if (!user) {
+      throw new Common.Classes.AppError("User not found", 404);
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserById = async (id: string, needPassword?: boolean) => {
+  try {
+    const query = needPassword
+      ? Models.User.findById(id)
+      : Models.User.findById(id).select("-password");
+
+    const user = await query.lean();
+
+    if (!user) {
+      throw new Common.Classes.AppError("User not found", 404);
+    }
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
