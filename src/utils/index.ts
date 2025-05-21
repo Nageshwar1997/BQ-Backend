80b3c3f6ded03daf29d3c1d9943733009616d557
nageshwarp1997 <nageshwar@ctruh.com>
2025-05-21 17:32:12 +0530

    Added Optional String Field validations

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,58 +29,122 @@ export const validateZodString = ({
   blockMultipleSpaces,
   parentField,
   customRegex,
+  isOptional = false,
 }: ZodStringProps) => {
   const nestedField = parentField ? `${parentField}.${field}` : field;
 
-  let schema = z
+  const messages = {
+    required: `The '${nestedField}' field is required.`,
+    invalid_type: `The '${nestedField}' field must be a string.`,
+    non_empty: `The '${nestedField}' field cannot be empty.`,
+    min: `The '${nestedField}' field must be at least ${min} characters.`,
+    max: `The '${nestedField}' field must not exceed ${max} characters.`,
+    multiple_spaces: `The '${nestedField}' field must not contain multiple consecutive spaces.`,
+    single_space: `The '${nestedField}' field must not contain any spaces.`,
+    custom: customRegex?.message
+      ? `The '${nestedField}' field ${customRegex?.message}.`
+      : `The '${nestedField}' field does not match the required format.`,
+  };
+
+  let requiredSchema = z
     .string({
-      required_error: `The '${nestedField}' field is required.`,
-      invalid_type_error: `The '${nestedField}' field must be a string.`,
+      required_error: messages.required,
+      invalid_type_error: messages.invalid_type,
     })
     .trim();
 
   if (nonEmpty) {
-    schema = schema.nonempty({
-      message: `The '${nestedField}' field cannot be empty.`,
-    });
+    requiredSchema = requiredSchema.nonempty({ message: messages.non_empty });
   }
 
   if (min !== undefined) {
-    schema = schema.min(
-      min,
-      `The '${nestedField}' field must be at least ${min} characters long.`
-    );
+    requiredSchema = requiredSchema.min(min, messages.min);
   }
 
   if (max !== undefined) {
-    schema = schema.max(
-      max,
-      `'${nestedField}' must not exceed ${max} characters.`
-    );
+    requiredSchema = requiredSchema.max(max, messages.max);
   }
 
   if (blockMultipleSpaces) {
-    schema = schema.regex(
+    requiredSchema = requiredSchema.regex(
       singleSpaceRegex,
-      `The '${nestedField}' field must not contain multiple consecutive spaces.`
+      messages.multiple_spaces
     );
   }
 
   if (blockSingleSpace) {
-    schema = schema.regex(
-      noSpaceRegex,
-      `The '${nestedField}' field must not contain any spaces.`
-    );
+    requiredSchema = requiredSchema.regex(noSpaceRegex, messages.single_space);
   }
 
-  if (customRegex) {
-    const { regex, message } = customRegex;
-    if (regex && message) {
-      schema = schema.regex(regex, message);
-    }
+  if (customRegex && customRegex.regex) {
+    requiredSchema = requiredSchema.regex(customRegex.regex, messages.custom);
   }
 
-  return schema;
+  const optionalSchema = z
+    .string()
+    .trim()
+    .default("")
+    .optional()
+    .superRefine((val, ctx) => {
+      if (typeof val !== "string") {
+        ctx.addIssue({
+          code: z.ZodIssueCode.invalid_type,
+          expected: "string",
+          received: typeof val,
+          path: ctx.path,
+          message: messages.invalid_type,
+        });
+        return;
+      }
+
+      if (min !== undefined && val.length < min && val !== "") {
+        ctx.addIssue({
+          code: z.ZodIssueCode.too_small,
+          type: "string",
+          minimum: min,
+          path: ctx.path,
+          inclusive: true,
+          message: messages.min,
+        });
+      }
+
+      if (max !== undefined && val.length > max) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.too_big,
+          type: "string",
+          maximum: max,
+          path: ctx.path,
+          inclusive: true,
+          message: messages.max,
+        });
+      }
+
+      if (blockMultipleSpaces && singleSpaceRegex.test(val)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ctx.path,
+          message: messages.multiple_spaces,
+        });
+      }
+
+      if (blockSingleSpace && noSpaceRegex.test(val)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ctx.path,
+          message: messages.single_space,
+        });
+      }
+
+      if (customRegex && customRegex?.regex && !customRegex.regex.test(val)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ctx.path,
+          message: messages.custom,
+        });
+      }
+    });
+
+  return isOptional ? optionalSchema : requiredSchema;
 };
 
 export const validateZodNumber = ({
