9c2428165c2930fded3d9d4317c95b5ff8aba7f9
nageshwarp1997 <nageshwar@ctruh.com>
2025-06-15 14:21:56 +0530

    `Added PATCH /product/update/:productId endpoint with authorization and file validation`

--- src/modules/product/routes/product/index.ts
+++ src/modules/product/routes/product/index.ts
@@ -46,3 +46,30 @@ productRouter.post(
   "/product/:productId",
   ResponseMiddleware.catchAsync(getProductByIdController)
 );
+
+productRouter.patch(
+  "/product/update/:productId",
+  AuthMiddleware.authorization(["ADMIN", "MASTER", "SELLER"]),
+  MulterMiddleware.validateFiles({ type: "any" }),
+  RequestMiddleware.checkEmptyRequest({ body: true }),
+  JSONParseMiddleware.JSONParse({
+    fieldsToParse: [
+      "categoryLevelOne",
+      "categoryLevelTwo",
+      "categoryLevelThree",
+      "shades",
+    ],
+  }),
+  (req, res) => {
+    console.log(
+      "updated-with-files-shades",
+      req.body["updated-with-files-shades"]
+    );
+    // console.log("req.body", req.body);
+    console.log("req.files", req.files);
+    console.log("req.params", req.params);
+    console.log("req.query", req.query);
+    console.log("req.file", req.file);
+    res.success(200, "Product updated successfully", req.body);
+  }
+);
