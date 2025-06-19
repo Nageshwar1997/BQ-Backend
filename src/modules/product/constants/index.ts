48f77975d7456cd8055cad92b38362fcc5ac44da
nageshwarp1997 <nageshwar@ctruh.com>
2025-06-19 18:15:08 +0530

    `Added ProductProps import and possibleUpdateProductFields constant`

--- src/modules/product/constants/index.ts
+++ src/modules/product/constants/index.ts
@@ -1,7 +1,19 @@
-import { ProductPopulateFieldsProps } from "../types";
+import { ProductPopulateFieldsProps, ProductProps } from "../types";
 
 export const productPopulateFields: ProductPopulateFieldsProps = {
   category: ["name", "category", "level", "parentCategory"],
   shades: ["shadeName", "colorCode", "images", "stock"],
   seller: ["firstName", "lastName", "phoneNumber", "email", "profilePic"],
 };
+
+export const possibleUpdateProductFields: (keyof ProductProps)[] = [
+  "title",
+  "brand",
+  "originalPrice",
+  "sellingPrice",
+  "description",
+  "howToUse",
+  "ingredients",
+  "additionalDetails",
+  "totalStock",
+];
