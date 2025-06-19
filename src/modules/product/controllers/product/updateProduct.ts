de92576c1957821262e39c76f3e75f755d1dda29
nageshwarp1997 <nageshwar@ctruh.com>
2025-06-19 21:11:12 +0530

    `Added user permission check, updated shade stock calculation, and improved error handling in updateProductController`

--- src/modules/product/controllers/product/updateProduct.ts
+++ src/modules/product/controllers/product/updateProduct.ts
@@ -1,21 +1,27 @@
 import { Types } from "mongoose";
-import { Response, NextFunction } from "express";
+import { Response } from "express";
 import { AuthorizedRequest } from "../../../../types";
 import { Product, Shade } from "../../models";
 import { AppError } from "../../../../classes";
 import { MediaModule } from "../../..";
 import { PopulatedProduct, ProductProps, ShadeProps } from "../../types";
 import { findOrCreateCategory } from "../../services";
-import { isValidMongoId } from "../../../../utils";
+import { checkUserPermission, isValidMongoId } from "../../../../utils";
 import { possibleUpdateProductFields } from "../../constants";
 
 export const updateProductController = async (
   req: AuthorizedRequest,
-  res: Response,
-  next: NextFunction
+  res: Response
 ) => {
   const { productId } = req.params;
   isValidMongoId(productId, "Invalid Product Id", 400);
+
+  checkUserPermission({
+    userId: req.user?._id as string,
+    checkId: productId as string,
+    message: "You are not authorized to update this product",
+    statusCode: 403,
+  });
 
   const {
     title,
@@ -125,6 +131,11 @@ export const updateProductController = async (
   let uploadedCommonImages: string[] = [];
   let uploadedNewShadesImages: string[] = [];
   let uploadedUpdatedShadesImages: string[] = [];
+  let shadesTotalStock = existingProduct.totalStock;
+  let newShadeIds: Types.ObjectId[] = [];
+
+  let removedExistingShadesWithFileImages: string[] = [];
+  let removedExistingShadesWithOutFileImages: string[] = [];
 
   const newShadeImagesMap: Record<number, Express.Multer.File[]> = {};
   const updatedShadeImagesMap: Record<number, Express.Multer.File[]> = {};
@@ -177,173 +188,342 @@ export const updateProductController = async (
     ];
   }
 
-  console.log("removingShades", removingShades);
-
-  // Remove Shades
-  // if (removingShades?.length) {
-  //   const objectIds = removingShades.map(
-  //     (id: string) => new Types.ObjectId(id)
-  //   );
-  //   await Shade.deleteMany({ _id: { $in: objectIds } });
-  // }
-
   // New Added Shades
   if (newAddedShades?.length) {
-    const newShadesData = Array.isArray(newAddedShades)
-      ? newAddedShades
-      : [newAddedShades];
-    let newShadeIds: Types.ObjectId[] = [];
+    try {
+      const newShadesData = Array.isArray(newAddedShades)
+        ? newAddedShades
+        : [newAddedShades];
 
-    const missingShadeErrors: string[] = [];
-    let totalStock = existingProduct.totalStock;
+      const missingShadeErrors: string[] = [];
 
-    newShadesData.forEach((shade, idx) => {
-      if (!(newShadeImagesMap[idx] && newShadeImagesMap[idx].length > 0)) {
-        const shadeName = shade.shadeName || `Unknown Shade at index ${idx}`;
-        missingShadeErrors.push(
-          `Shade: '${shadeName}' At least 1 image is required`
+      newShadesData.forEach((shade, idx) => {
+        if (!(newShadeImagesMap[idx] && newShadeImagesMap[idx].length > 0)) {
+          const shadeName = shade.shadeName || `Unknown Shade at index ${idx}`;
+          missingShadeErrors.push(
+            `Shade: '${shadeName}' At least 1 image is required`
+          );
+        }
+      });
+
+      if (missingShadeErrors.length > 0) {
+        const errorMessage = missingShadeErrors
+          .map(
+            (msg, i) =>
+              `${missingShadeErrors.length > 1 ? `${i + 1}). ` : ""}${msg}${
+                i === missingShadeErrors.length - 1 ? "." : ""
+              }`
+          )
+          .join(", ");
+        throw new AppError(errorMessage, 400);
+      }
+
+      // Upload shade images
+      const enrichedShades = await Promise.all(
+        newShadesData.map(async (shade, idx) => {
+          const shadeFiles = newShadeImagesMap[idx] || [];
+
+          shadesTotalStock += shade.stock;
+          const uploadedShadeImagesResult =
+            await MediaModule.Utils.multipleImagesUploader({
+              files: shadeFiles,
+              folder: `Products/${title ?? existingProduct.title}/Shades/${
+                shade.shadeName
+              }`,
+              cloudinaryConfigOption: "product",
+            });
+
+          const images = uploadedShadeImagesResult.map((img) => img.secure_url);
+          uploadedNewShadesImages.push(...images);
+
+          return {
+            ...shade,
+            images,
+          };
+        })
+      );
+
+      newShadeIds = await Promise.all(
+        enrichedShades.map(async (shade) => {
+          const newShade = await Shade.create(shade);
+          return new Types.ObjectId(newShade._id);
+        })
+      );
+
+      updateBody.shades = [
+        ...existingProduct.shades
+          .filter(
+            (sh) => sh._id && !removingShades?.includes(sh._id.toString())
+          )
+          .map((sh) => new Types.ObjectId(sh._id)),
+        ...newShadeIds,
+      ];
+    } catch (error) {
+      if (uploadedNewShadesImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedNewShadesImages,
+          "product"
         );
       }
-    });
-
-    if (missingShadeErrors.length > 0) {
-      const errorMessage = missingShadeErrors
-        .map(
-          (msg, i) =>
-            `${missingShadeErrors.length > 1 ? `${i + 1}). ` : ""}${msg}${
-              i === missingShadeErrors.length - 1 ? "." : ""
-            }`
-        )
-        .join(", ");
-      throw new AppError(errorMessage, 400);
+      if (uploadedCommonImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedCommonImages,
+          "product"
+        );
+      }
+      throw new AppError("Failed to create shades", 500);
     }
-
-    // Upload shade images
-    const enrichedShades = await Promise.all(
-      newShadesData.map(async (shade, idx) => {
-        const shadeFiles = newShadeImagesMap[idx] || [];
-
-        totalStock += shade.stock;
-        const uploadedShadeImagesResult =
-          await MediaModule.Utils.multipleImagesUploader({
-            files: shadeFiles,
-            folder: `Products/${title ?? existingProduct.title}/Shades/${
-              shade.shadeName
-            }`,
-            cloudinaryConfigOption: "product",
-          });
-
-        const images = uploadedShadeImagesResult.map((img) => img.secure_url);
-        uploadedNewShadesImages.push(...images);
-
-        return {
-          ...shade,
-          images,
-        };
-      })
-    );
-
-    newShadeIds = await Promise.all(
-      enrichedShades.map(async (shade) => {
-        const newShade = await Shade.create(shade);
-        return new Types.ObjectId(newShade._id);
-      })
-    );
-
-    console.log("newShadeIds", newShadeIds);
-
-    updateBody.shades = [
-      ...existingProduct.shades
-        .filter((sh) => sh._id && !removingShades?.includes(sh._id.toString()))
-        .map((sh) => new Types.ObjectId(sh._id)),
-      ...newShadeIds,
-    ];
   }
 
   if (updatedShadeWithFiles?.length) {
-    const updatedShadesData = Array.isArray(updatedShadeWithFiles)
-      ? updatedShadeWithFiles
-      : [updatedShadeWithFiles];
+    try {
+      const updatedShadesData = Array.isArray(updatedShadeWithFiles)
+        ? updatedShadeWithFiles
+        : [updatedShadeWithFiles];
 
-    const missingUpdatedShadeErrors: string[] = [];
-    // Total stock ka baad me dekhna he
-    // let totalStock = existingProduct.totalStock;
+      const missingUpdatedShadeErrors: string[] = [];
 
-    updatedShadesData.forEach((shade, idx) => {
-      if (
-        !(updatedShadeImagesMap[idx] && updatedShadeImagesMap[idx].length > 0)
-      ) {
-        const currentShade = existingProduct.shades.find(
-          (sh) => sh?._id?.toString() === shade._id?.toString()
-        );
+      updatedShadesData.forEach((shade, idx) => {
+        if (
+          !(updatedShadeImagesMap[idx] && updatedShadeImagesMap[idx].length > 0)
+        ) {
+          const currentShade = existingProduct.shades.find(
+            (sh) => sh?._id?.toString() === shade._id?.toString()
+          );
 
-        const shadeName =
-          shade.shadeName ??
-          (currentShade?.shadeName || `Unknown Shade at index ${idx}`);
-        missingUpdatedShadeErrors.push(
-          `Shade: '${shadeName}' At least 1 image is required`
+          if (!currentShade) {
+            throw new AppError(`Shade not found with id: ${shade._id}`, 404);
+          }
+
+          const shadeName = shade.shadeName ?? currentShade?.shadeName;
+          missingUpdatedShadeErrors.push(
+            `Shade: '${shadeName}' At least 1 image is required`
+          );
+        }
+      });
+
+      if (missingUpdatedShadeErrors.length > 0) {
+        const errorMessage = missingUpdatedShadeErrors
+          .map(
+            (msg, i) =>
+              `${
+                missingUpdatedShadeErrors.length > 1 ? `${i + 1}). ` : ""
+              }${msg}${i === missingUpdatedShadeErrors.length - 1 ? "." : ""}`
+          )
+          .join(", ");
+        throw new AppError(errorMessage, 400);
+      }
+
+      const enrichedUpdatedShades = await Promise.all(
+        updatedShadesData.map(async (shade, idx) => {
+          const shadeFiles = updatedShadeImagesMap[idx] || [];
+          const currentShade = existingProduct.shades.find(
+            (sh) => sh?._id?.toString() === shade._id?.toString()
+          );
+          if (!currentShade) return;
+          // totalStock += shade.stock;
+          const uploadedShadeImagesResult =
+            await MediaModule.Utils.multipleImagesUploader({
+              files: shadeFiles,
+              folder: `Products/${title || existingProduct.title}/Shades/${
+                shade.shadeName || currentShade.shadeName
+              }`,
+              cloudinaryConfigOption: "product",
+            });
+
+          const images = uploadedShadeImagesResult.map((img) => img.secure_url);
+          uploadedUpdatedShadesImages.push(...images);
+
+          const currentShadeRemovingImages =
+            removingShadeImageUrls?.find(
+              (sh: { _id: string; urls: string[] }) =>
+                sh._id.toString() === shade._id.toString()
+            )?.urls || [];
+
+          removedExistingShadesWithFileImages.push(
+            ...currentShadeRemovingImages
+          );
+
+          const existingShadeImgUrls =
+            currentShade.images.filter(
+              (img) => !currentShadeRemovingImages.includes(img)
+            ) || [];
+
+          if (shade.stock) {
+            shadesTotalStock +=
+              shadesTotalStock - currentShade.stock + shade.stock;
+          }
+
+          return {
+            ...shade,
+            images: [...existingShadeImgUrls, ...images],
+          };
+        })
+      );
+      await Promise.all(
+        enrichedUpdatedShades.map(async (shade) => {
+          await Shade.findByIdAndUpdate(
+            { _id: shade._id },
+            { $set: { ...shade } }
+          );
+        })
+      );
+    } catch (error) {
+      if (uploadedUpdatedShadesImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedUpdatedShadesImages,
+          "product"
         );
       }
-    });
+      if (uploadedNewShadesImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedNewShadesImages,
+          "product"
+        );
+      }
+      if (uploadedCommonImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedCommonImages,
+          "product"
+        );
+      }
+      if (newShadeIds.length) {
+        await Shade.deleteMany({ _id: { $in: newShadeIds } });
+      }
 
-    if (missingUpdatedShadeErrors.length > 0) {
-      const errorMessage = missingUpdatedShadeErrors
-        .map(
-          (msg, i) =>
-            `${
-              missingUpdatedShadeErrors.length > 1 ? `${i + 1}). ` : ""
-            }${msg}${i === missingUpdatedShadeErrors.length - 1 ? "." : ""}`
-        )
-        .join(", ");
-      throw new AppError(errorMessage, 400);
+      throw error;
     }
-
-    const enrichedUpdatedShades = await Promise.all(
-      updatedShadesData.map(async (shade, idx) => {
-        const shadeFiles = updatedShadeImagesMap[idx] || [];
-        const currentShade = existingProduct.shades.find(
-          (sh) => sh?._id?.toString() === shade._id?.toString()
-        );
-        if (!currentShade) return;
-        // totalStock += shade.stock;
-        const uploadedShadeImagesResult =
-          await MediaModule.Utils.multipleImagesUploader({
-            files: shadeFiles,
-            folder: `Products/${title || existingProduct.title}/Shades/${
-              shade.shadeName || currentShade.shadeName
-            }`,
-            cloudinaryConfigOption: "product",
-          });
-
-        const images = uploadedShadeImagesResult.map((img) => img.secure_url);
-        uploadedUpdatedShadesImages.push(...images);
-
-        return {
-          ...shade,
-          images: [...currentShade.images, ...images],
-        };
-      })
-    );
-
-    await Promise.all(
-      enrichedUpdatedShades.map(async (shade) => {
-        await Shade.findByIdAndUpdate(
-          { _id: shade._id },
-          { $set: { ...shade } }
-        );
-      })
-    );
   }
 
   if (updatedShadeWithoutFiles?.length) {
-    await Promise.all(
-      updatedShadeWithoutFiles.map(async (shade: Partial<ShadeProps>) => {
-        const sh = await Shade.findByIdAndUpdate(
-          { _id: shade._id },
-          { $set: { ...shade } }
+    try {
+      await Promise.all(
+        updatedShadeWithoutFiles.map(async (shade: Partial<ShadeProps>) => {
+          const currentShade = existingProduct.shades.find(
+            (sh) => sh?._id?.toString() === shade._id?.toString()
+          );
+
+          if (!currentShade) {
+            throw new AppError(`Shade not found with id: ${shade._id}`, 404);
+          }
+
+          const currentShadeRemovingImages =
+            removingShadeImageUrls?.find(
+              (sh: { _id: string; urls: string[] }) =>
+                sh._id.toString() === shade._id?.toString()
+            )?.urls || [];
+
+          removedExistingShadesWithOutFileImages.push(
+            ...currentShadeRemovingImages
+          );
+
+          if (shade.stock) {
+            shadesTotalStock +=
+              shadesTotalStock - currentShade.stock + shade.stock;
+          }
+
+          await Shade.findByIdAndUpdate(
+            { _id: shade._id },
+            { $set: { ...shade } }
+          );
+        })
+      );
+    } catch (error) {
+      if (uploadedUpdatedShadesImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedUpdatedShadesImages,
+          "product"
         );
-      })
-    );
+      }
+      if (uploadedNewShadesImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedNewShadesImages,
+          "product"
+        );
+      }
+      if (uploadedCommonImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          uploadedCommonImages,
+          "product"
+        );
+      }
+      if (removedExistingShadesWithFileImages.length) {
+        await MediaModule.Utils.multipleImagesRemover(
+          removedExistingShadesWithFileImages,
+          "product"
+        );
+      }
+      if (newShadeIds.length) {
+        await Shade.deleteMany({ _id: { $in: newShadeIds } });
+      }
+      throw error;
+    }
   }
-  res.success(200, "Product updated successfully");
+
+  try {
+    if (totalStock ?? existingProduct.totalStock !== shadesTotalStock) {
+      throw new AppError(
+        "Total stock should be equal to sum of shades stock",
+        400
+      );
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      { _id: productId },
+      updateBody,
+      { new: true }
+    );
+
+    // Remove Shades
+    let removedShadeImages: string[] = [];
+    if (removingShades?.length) {
+      const objectIds = removingShades.map(
+        (id: string) => new Types.ObjectId(id)
+      );
+
+      // Fetch shades to be removed
+      const shadesToDelete = await Shade.find({ _id: { $in: objectIds } });
+
+      removedShadeImages =
+        shadesToDelete.flatMap((shade) => shade.images) || [];
+      await Shade.deleteMany({ _id: { $in: objectIds } });
+    }
+
+    res.success(200, "Product updated successfully", { product });
+  } catch (error) {
+    if (uploadedUpdatedShadesImages.length) {
+      await MediaModule.Utils.multipleImagesRemover(
+        uploadedUpdatedShadesImages,
+        "product"
+      );
+    }
+    if (uploadedNewShadesImages.length) {
+      await MediaModule.Utils.multipleImagesRemover(
+        uploadedNewShadesImages,
+        "product"
+      );
+    }
+    if (uploadedCommonImages.length) {
+      await MediaModule.Utils.multipleImagesRemover(
+        uploadedCommonImages,
+        "product"
+      );
+    }
+    if (removedExistingShadesWithFileImages.length) {
+      await MediaModule.Utils.multipleImagesRemover(
+        removedExistingShadesWithFileImages,
+        "product"
+      );
+    }
+    if (removedExistingShadesWithOutFileImages.length) {
+      await MediaModule.Utils.multipleImagesRemover(
+        removedExistingShadesWithOutFileImages,
+        "product"
+      );
+    }
+    if (newShadeIds.length) {
+      await Shade.deleteMany({ _id: { $in: newShadeIds } });
+    }
+    throw error;
+  }
 };
