77e3b9a161a5bb44580bc7f125270e36e32da446
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-17 10:15:13 +0530

    `Update regexes in constants/regex/index.ts: modified singleSpace regex to use {2,} and added 's' flag`

--- src/constants/regex/index.ts
+++ src/constants/regex/index.ts
@@ -17,7 +17,7 @@ export const regexes: Record<RegexKeys, RegExp> = {
   validName: /^(?!.*\d)(?!.* {2})([A-Za-z]+( [A-Za-z]+)*)$/,
   validEmail:
     /^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(-?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})+$/,
-  singleSpace: /^(?!.*\s{2,}).*$/,
+  singleSpace: /^(?!.* {2,}).*$/s,
   validPassword:
     /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])(?=\S.*$).{6,20}$/,
   validHexColorCode:
