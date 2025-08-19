5330072b27c13d9758217187936f172282b676c9
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-19 21:06:18 +0530

    removed spaces

--- src/modules/media/schemas/homeVideo.ts
+++ src/modules/media/schemas/homeVideo.ts
@@ -3,40 +3,13 @@ import { HomeVideoProps } from "../types";
 
 export const homeVideoSchema = new Schema<HomeVideoProps>(
   {
-    m3u8Url: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    originalUrl: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    posterUrl: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    public_id: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+    m3u8Url: { type: String, required: true, trim: true },
+    originalUrl: { type: String, required: true, trim: true },
+    posterUrl: { type: String, required: true, trim: true },
+    public_id: { type: String, required: true, trim: true },
+    title: { type: String, required: true, trim: true },
     duration: { type: Number, required: true },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
+  { timestamps: true, versionKey: false }
 );
