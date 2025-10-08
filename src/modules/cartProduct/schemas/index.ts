3d7b368a6ef13161a0d15240794f1b0392165ac2
nageshwarp1997 <nageshwar@ctruh.com>
2025-10-08 12:08:53 +0530

    Cart Product Schema made reusable

--- src/modules/cartProduct/schemas/index.ts
+++ src/modules/cartProduct/schemas/index.ts
@@ -1,12 +1,16 @@
 import { Schema } from "mongoose";
 import { TCartProduct } from "../types";
 
+export const cartProductBaseFields = {
+  product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  shade: { type: Schema.Types.ObjectId, ref: "Shade", default: null },
+  quantity: { type: Number, required: true, min: 1, max: 5 },
+};
+
 export const cartProductSchema = new Schema<TCartProduct>(
   {
+    ...cartProductBaseFields,
     cart: { type: Schema.Types.ObjectId, ref: "Cart", required: true },
-    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-    shade: { type: Schema.Types.ObjectId, ref: "Shade" },
-    quantity: { type: Number, required: true, min: 1, max: 5 },
   },
-  { versionKey: false }
+  { versionKey: false, timestamps: true }
 );
