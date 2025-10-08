c71ab0c7954422ae6d6f1aed40441b80262bfa42
nageshwarp1997 <nageshwar@ctruh.com>
2025-10-08 08:44:59 +0530

    Created Service to get user cart

---
+++ src/modules/cart/services/getUserCart.ts
@@ -0,0 +1,33 @@
+import { AuthenticatedRequest } from "../../../types";
+import { AppError } from "../../../classes";
+import { Cart } from "../models";
+
+export const getUserCart = async (req: AuthenticatedRequest) => {
+  const userId = req.user?._id;
+  const cart = await Cart.findOne({ user: userId })
+    .populate({
+      path: "products", // All Products in the cart
+      populate: [
+        {
+          path: "product", // Product Path
+          model: "Product", // Product Model
+          select:
+            "title brand originalPrice sellingPrice discount commonImages totalStock", // Required Product Fields
+          options: { select: { commonImages: { $slice: 1 } } }, // Required Product Options
+        },
+        {
+          path: "shade",
+          model: "Shade",
+          select: "shadeName colorCode images stock", // Required Shade Fields
+          options: { select: { images: { $slice: 1 } } }, // Required Shade Options
+        },
+      ],
+    })
+    .lean();
+
+  if (!cart) {
+    throw new AppError("Cart not found", 404);
+  }
+
+  return cart;
+};
