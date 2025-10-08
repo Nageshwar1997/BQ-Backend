7fa9a05cbac1569ee4af6b128f52093c6eedb94a
nageshwarp1997 <nageshwar@ctruh.com>
2025-10-08 12:06:02 +0530

    Address schema optimized

--- src/modules/address/index.ts
+++ src/modules/address/index.ts
@@ -1,2 +1,4 @@
 export * as Types from "./types";
 export * as Routes from "./routes";
+export * as Schemas from "./schemas";
+export * as Models from "./models";
--- src/modules/address/schemas/index.ts
+++ src/modules/address/schemas/index.ts
@@ -2,31 +2,35 @@ import { Schema } from "mongoose";
 import { IAddress, IUserAddresses } from "../types";
 import { ADDRESS_TYPES, ALLOWED_COUNTRIES } from "../constants";
 
+export const addressBaseFields = {
+  address: { type: String, required: true },
+  landmark: { type: String },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  pinCode: { type: String, required: true, minlength: 6, maxlength: 6 },
+  country: {
+    type: String,
+    required: true,
+    enum: ALLOWED_COUNTRIES,
+    default: "India",
+  },
+  phoneNumber: { type: String, required: true },
+  altPhoneNumber: { type: String, default: "" },
+  gst: { type: String, default: "" },
+  type: {
+    type: String,
+    required: true,
+    enum: ADDRESS_TYPES,
+    default: "both",
+  },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true },
+};
+
 export const addressSchema = new Schema<IAddress>(
   {
-    address: { type: String, required: true },
-    landmark: { type: String },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    pinCode: { type: String, required: true, minlength: 6, maxlength: 6 },
-    country: {
-      type: String,
-      required: true,
-      enum: ALLOWED_COUNTRIES,
-      default: "India",
-    },
-    phoneNumber: { type: String, required: true },
-    altPhoneNumber: { type: String, default: "" },
-    gst: { type: String, default: "" },
-    type: {
-      type: String,
-      required: true,
-      enum: ADDRESS_TYPES,
-      default: "both",
-    },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, lowercase: true },
+    ...addressBaseFields,
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true, versionKey: false }
