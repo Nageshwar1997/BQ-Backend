a5fee1a812652e619be2a4a99c3ead9c322737c7
nageshwarp1997 <nageshwar@ctruh.com>
2025-11-16 14:57:01 +0530

    `Added population and selection of fields for blog publisher in getBlogByIdController`

--- src/modules/blog/controllers/getBlogById.ts
+++ src/modules/blog/controllers/getBlogById.ts
@@ -7,7 +7,12 @@ export const getBlogByIdController = async (req: Request, res: Response) => {
   const { id } = req.params;
   isValidMongoId(id, "Invalid Blog Id provided", 404);
 
-  const blog = await Blog.findById(id).lean();
+  const blog = await Blog.findById(id)
+    .populate({
+      path: "publisher",
+      select: "firstName lastName profilePic -_id", // only these fields
+    })
+    .lean();
 
   if (!blog) {
     throw new AppError("Blog not found", 404);
