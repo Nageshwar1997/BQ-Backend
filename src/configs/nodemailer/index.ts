d4ed8c79784031921c1c48229ddc0bc974fadc25
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-06 12:06:39 +0530

    `Update nodemailer config to use CTRUH mail credentials`

--- src/configs/nodemailer/index.ts
+++ src/configs/nodemailer/index.ts
@@ -1,21 +1,14 @@
 import nodemailer from "nodemailer";
-import {
-  CTRUH_MAIL_HOST,
-  CTRUH_MAIL_PASS,
-  CTRUH_MAIL_USER,
-  IS_DEV,
-  MAIL_PORT,
-  MY_MAIL_HOST,
-  MY_MAIL_PASS,
-  MY_MAIL_USER,
-} from "../../envs";
+import { CTRUH_MAIL_HOST, CTRUH_MAIL_PASS, CTRUH_MAIL_USER } from "../../envs";
 
 export const transporterConfig = nodemailer.createTransport({
-  host: IS_DEV === "true" ? MY_MAIL_HOST : CTRUH_MAIL_HOST,
-  port: Number(MAIL_PORT),
-  secure: false,
+  host: CTRUH_MAIL_HOST,
+  port: 465,
+  secure: true,
   auth: {
-    user: IS_DEV === "true" ? MY_MAIL_USER : CTRUH_MAIL_USER,
-    pass: IS_DEV === "true" ? MY_MAIL_PASS : CTRUH_MAIL_PASS,
+    user: CTRUH_MAIL_USER,
+    pass: CTRUH_MAIL_PASS,
   },
+  logger: true,
+  debug: true,
 });
