ebf2d13e87e110ffa4602423c1be949274400760
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-06 21:24:11 +0530

    feat: implement core service and error handling classes including MailService, RedisService, and custom error builders

---
+++ src/Classes/index.ts
@@ -0,0 +1,5 @@
+export * from "./AppSuccess.Class";
+export * from "./AppError.Class";
+export * from "./MailService.Class";
+export * from "./RedisService.Class";
+export * from "./ErrorBuilder.Class";
