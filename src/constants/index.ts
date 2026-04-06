4498841527afe81efe40ae38d51d435f259fd8e8
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-06 14:49:05 +0530

    feat: export consolidated application constants via index file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,8 +3,6 @@ import { FileConstants } from "./File.Constant";
 import { Regex } from "./Regex.Constant.ts";
 import { ZodConstants } from "./Zod.Constant.ts";
 
-export * from "./Zod.Constant.ts";
-
 export const Constants = {
   Common: CommonConstants,
   File: FileConstants,
